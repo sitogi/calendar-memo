8246ce7c3a0f03d64ab21a3b4e6f087c3dc9c08c
sitogi <takano162@gmail.com>
2024-02-08 22:50:44 +0900

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { JSX } from 'react';
 
+import { MonthCalendar } from '~/features/MonthCalendar';
+
 export const App = (): JSX.Element => {
-  return (
-    <div className="grid place-content-center w-dvw h-dvh bg-fuchsia-50">
-      <h1 className="text-blue-300 text-3xl font-bold underline">Hello world!</h1>
-    </div>
-  );
+  return <MonthCalendar />;
 };
---
+++ src/features/MonthCalendar/index.tsx
@@ -0,0 +1,9 @@
+import { JSX } from 'react';
+
+export const MonthCalendar = (): JSX.Element => {
+  return (
+    <div className="grid place-content-center w-dvw h-dvh bg-fuchsia-50">
+      <h1 className="text-blue-300 text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
