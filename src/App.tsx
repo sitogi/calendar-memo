d2265c413fb1db673dfc654b8f1cafa7f79c0a94
sitogi <takano162@gmail.com>
2024-02-08 13:24:07 +0900

    Add @dnd-kit/core and @dnd-kit/sortable

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { JSX } from 'react';
 
 export const App = (): JSX.Element => {
   return (
-    <div className="grid place-content-center w-dvw h-dvh">
+    <div className="grid place-content-center w-dvw h-dvh bg-fuchsia-50">
       <h1 className="text-blue-300 text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
