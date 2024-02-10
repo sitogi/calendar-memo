be9801a12602cbaa67990bf15b7affa35c17797c
sitogi <takano162@gmail.com>
2024-02-11 00:09:58 +0900

    Fix the width of date

--- src/features/MonthCalendar/index.tsx
+++ src/features/MonthCalendar/index.tsx
@@ -1,5 +1,6 @@
 import { JSX, useState } from 'react';
 
+import { ArrowLeft, ArrowRight, CalendarHeart, Settings } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 import { Button, buttonVariants } from '~/components/ui/button';
@@ -23,29 +24,34 @@ export const MonthCalendar = (): JSX.Element => {
 
   return (
     <div className="h-screen flex flex-col">
-      <div className="flex items-center justify-between h-16 px-16">
+      <div className="flex items-center justify-between h-16 px-16 border-b mb-4">
         <Link to="/month" className={buttonVariants({ variant: 'ghost' })}>
+          <CalendarHeart className="mr-2 h-4 w-4" />
           Today
         </Link>
         <div className="flex items-center justify-center gap-16">
           <Link
-            to={`/month/${month === 1 ? year - 1 : year}/${month === 1 ? 12 : month - 1}`}
             className={buttonVariants({ variant: 'ghost' })}
+            to={`/month/${month === 1 ? year - 1 : year}/${month === 1 ? 12 : month - 1}`}
           >
+            <ArrowLeft className="mr-2 h-4 w-4" />
             Prev
           </Link>
-          <h1 className="text-center text-3xl">{`${year} - ${month}`}</h1>
+          <h1 className="text-center text-3xl w-36">{`${year} - ${month}`}</h1>
           <Link
-            to={`/month/${month === 12 ? year + 1 : year}/${month === 12 ? 1 : month + 1}`}
             className={buttonVariants({ variant: 'ghost' })}
+            to={`/month/${month === 12 ? year + 1 : year}/${month === 12 ? 1 : month + 1}`}
           >
             Next
+            <ArrowRight className="ml-2 h-4 w-4" />
           </Link>
         </div>
         <div>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="ghost">Settings</Button>
+              <Button variant="ghost">
+                <Settings className="mr-2 h-4 w-4" /> Settings
+              </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56">
               <DropdownMenuLabel>Week origin</DropdownMenuLabel>
