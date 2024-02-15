d2c952feb27b26ebfeadfd891cf9318fdea12b79
sitogi <takano162@gmail.com>
2024-02-15 21:51:06 +0900

    全部 today になってたので修正

--- src/components/ui/buttonLink.tsx
+++ src/components/ui/buttonLink.tsx
@@ -1,14 +1,13 @@
 import { ComponentProps, JSX, ReactNode } from 'react';
-import { CalendarHeart } from 'lucide-react';
 import { buttonVariants } from '~/components/ui/button';
+import { cn } from '~/lib/utils';
 
 type Props = { children: ReactNode } & ComponentProps<'a'>;
 
 export const ButtonLink = ({ children, ...linkProps }: Props): JSX.Element => {
   return (
-    <a {...linkProps} className={buttonVariants({ variant: 'ghost' })}>
-      <CalendarHeart className="mr-2 h-4 w-4" />
-      Today
+    <a {...linkProps} className={cn(linkProps.className, buttonVariants({ variant: 'ghost' }))}>
+      {children}
     </a>
   );
 };
